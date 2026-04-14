6943b485002ea209b82f4b439051d1bdb850e2dd
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-04-14 15:08:17 +0530

    Create MoveDemo2.java

---
+++ Day-19/MoveDemo2.java
@@ -0,0 +1,34 @@
+package jdbc;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MoveDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "c##batch9am", "nareshit");
+			System.out.println("Connected..");
+			
+			Statement st = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+			ResultSet rs =st.executeQuery("SELECT studentid, studentname, marks FROM student");
+			
+			rs.absolute(2);
+			
+			rs.updateInt(3, 55);
+			rs.updateRow();
+			System.out.println("one row updated..");
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
+	}
+
+}
