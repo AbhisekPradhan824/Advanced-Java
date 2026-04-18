906700dee5a410ecc800653d4dce6cd0b8cd940d
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-04-18 21:02:08 +0530

    Update FirstServlet.java

--- Day-28/FirstServlet.java
+++ Day-28/FirstServlet.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import jakarta.servlet.*;
 
-public class FirstServlet{
+public class FirstServlet extends GenericServlet{
 public void service(ServletRequest req, ServletResponse res){
 	try{
 		PrintWriter pw = res.getWriter();
