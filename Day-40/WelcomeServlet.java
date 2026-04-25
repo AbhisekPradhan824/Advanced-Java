2f96d9b2464bcdf27207f59692cce6f3f04eda7c
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-04-25 23:21:48 +0530

    Create WelcomeServlet.java

---
+++ Day-40/WelcomeServlet.java
@@ -0,0 +1,31 @@
+package login;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/welcome")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String uname = request.getParameter("uname");
+		PrintWriter pw = response.getWriter();
+		
+		pw.println("<html><body bgcolor=cyan text = blue><center><h1>");
+		pw.println("Welcome "+ uname);
+		pw.println("</h1></center></body></html>");
+	}
+
+}
