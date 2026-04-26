0b9b2a0941ecc478485d765b7584564413a7b48c
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-04-26 10:48:57 +0530

    Create GetCookie.java

---
+++ Day-44/GetCookie.java
@@ -0,0 +1,42 @@
+package cookies;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class GetCookie
+ */
+@WebServlet("/get")
+public class GetCookie extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetCookie() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Cookie[] c = request.getCookies();
+		PrintWriter pw = response.getWriter();
+		pw.println("<html><body bgcolor=green text=yellow>");
+		pw.println("<h1><u>Selected Books </u></h1>");
+		for(Cookie c2 :c) {
+			pw.println(c2.getValue()+"<br>");
+		}
+		pw.println("</body></html>");
+	}
+
+}
