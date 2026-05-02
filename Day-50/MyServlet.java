fb9ccf800d2f67916e977100694180e0c84290b4
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-05-02 11:00:50 +0530

    Create MyServlet.java

---
+++ Day-50/MyServlet.java
@@ -0,0 +1,42 @@
+package scl;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/test")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ServletContext sc = request.getServletContext();
+		Connection con=(Connection) sc.getAttribute("oracle");
+		PrintWriter pw = response.getWriter();
+		if(con!=null) {
+		pw.println("Connection Established Successfully");}else {
+			pw.println("Error while establishing the Connection...");
+		}
+	}
+
+}
