ec7abad826059d4e9fe77329a612c2c2015b5d7c
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-05-04 23:14:13 +0530

    Update WelcomeServlet.java

--- Day-52/WelcomeServlet.java
+++ Day-52/WelcomeServlet.java
@@ -1,1 +1,13 @@
-
+package filters;
+import java.io.*;
+import jakarta.servlet.http.*;
+public  class WelcomeServlet extends HttpServlet{
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    String s1= request.getParameter("uname");
+    String s2 = request.getParameter("pword");
+    PrintWriter pw = response.getWriter();
+    pw.println("<html><body bgcolor=green text=yellow><h1>");
+    pw.println("Welcome"+s1);
+    pw.println("</h1></body></html>");
+  }
+}
